ce1ff3a995293141fb00d3fe5ea480978b1f87dd
mywonderland <mingruiyrh@gmail.com>
2016-10-18 13:47:58 -0400

    Create pgoMap.js

---
+++ pgoMap.js
@@ -0,0 +1,15 @@
+var map;
+  function loadMapScenario() {
+      map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+          credentials: 'AjCftuylaha9iD3UV9n2Gwa4DLAzWGoTfDKnqNZJVZzgn4A593v7T7WqWwNDXJYu'
+      });
+  } 
+  
+  
+# 1. Define pokemon data format, create pokemon data
+
+
+# 2. Create pokemon image on map
+
+
+# 3. Countdown refresh
